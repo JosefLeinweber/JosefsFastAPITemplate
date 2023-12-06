89cdc37bfd45be0114e15a8114539238f78525c7
Josef Leinweber <josef.leinweber@code.berlin>
2023-12-06 11:39:38 +0100

    fix(conftest): fixed 'event loop not closed' error by changing the scope of the async_client fixutre from session to function -> each test function will now have a new application (thats bad or?)

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 from src.main import initialize_application
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def event_loop():
     """Overrides pytest default function scoped event loop"""
     policy = asyncio.get_event_loop_policy()
@@ -21,7 +21,7 @@ def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 @pytest.mark.asyncio
 async def async_client():
     async with AsyncClient(app=initialize_application(), base_url="http://test") as async_client:
