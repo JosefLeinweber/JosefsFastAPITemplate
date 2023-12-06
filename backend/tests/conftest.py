364d88dc1afef7dff957cb96f33f77440ccecb78
Josef Leinweber <josef.leinweber@code.berlin>
2023-12-06 11:50:07 +0100

    fix(tests): changed scope of async_client fixture back to session and added the event_loop fixture to async_client as argument

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 from src.main import initialize_application
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def event_loop():
     """Overrides pytest default function scoped event loop"""
     policy = asyncio.get_event_loop_policy()
@@ -21,9 +21,9 @@ def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 @pytest.mark.asyncio
-async def async_client():
+async def async_client(event_loop) -> AsyncClient:
     async with AsyncClient(app=initialize_application(), base_url="http://test") as async_client:
         print("Client is ready")
         yield async_client
