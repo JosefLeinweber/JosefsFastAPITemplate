09cd7579ee5df20f20a6e9ab319977104e91b967
Josef Leinweber <josef.leinweber@code.berlin>
2023-12-06 12:01:14 +0100

    test(account): added detailed assert statments to the test_create_account

--- backend/tests/router_tests/test_account_router.py
+++ backend/tests/router_tests/test_account_router.py
@@ -1,3 +1,4 @@
+import loguru
 import pytest
 from httpx import AsyncClient
 
@@ -14,4 +15,13 @@ async def test_create_account(async_client: AsyncClient):
         "/v1/account",
         json={"username": "test", "email": "test@gmx.de", "password": "Test1234!"},
     )
+    loguru.logger.debug(response.json())
     assert response.status_code == 201
+    assert response.json()["username"] == "test"
+    assert response.json()["email"] == "test@gmx.de"
+    assert response.json()["password"] != "Test1234!"
+    assert response.json()["id"] is not None
+    assert response.json()["created_at"] is not None
+    assert response.json()["isAdmin"] is False
+    assert response.json()["isLoggedIn"] is True
+    assert response.json()["isVerified"] is False
